9a9fc0a6374611d13a1b3120960538333c7dbb22
Kramerale <aliceshoolz96@gmail.com>
2023-10-07 18:09:27 +0400

    PW 13 is ready for review2

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const appRouter = require('./routes/index');
-
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  useNewUrlParser: true,
-});
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(express.json()); // вместо бодипарсера
-app.use(express.urlencoded({ extended: true })); // вместо urlencoded из бодипарсера
+// app.use(express.json()); // вместо бодипарсера
+// app.use(express.urlencoded({ extended: true })); // вместо urlencoded из бодипарсера
+
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   req.user = {
@@ -22,6 +21,10 @@ app.use((req, res, next) => {
 });
 
 app.use(appRouter);
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
