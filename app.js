5e36c6a64e49f372137b35627430215b0600a1a9
Kramerale <aliceshoolz96@gmail.com>
2023-10-07 20:30:04 +0400

    PW 13 is ready for review3

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const appRouter = require('./routes/index');
 
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+});
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-// app.use(express.json()); // вместо бодипарсера
-// app.use(express.urlencoded({ extended: true })); // вместо urlencoded из бодипарсера
-
-app.use(bodyParser.json());
+app.use(express.json()); // вместо бодипарсера
+app.use(express.urlencoded({ extended: true })); // вместо urlencoded из бодипарсера
 
 app.use((req, res, next) => {
   req.user = {
@@ -21,10 +23,6 @@ app.use((req, res, next) => {
 });
 
 app.use(appRouter);
-
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  useNewUrlParser: true,
-});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,13 @@ const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 
+const notFound = 404;
+
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
+
+router.use((req, res) => {
+  res.status(notFound).send({message: `По адресу ${req.path} ничего не найдено`});
+});
 
 module.exports = router;
